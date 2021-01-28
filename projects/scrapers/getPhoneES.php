985353e6f9c24b8b1bdff90afe3988796971c5b1
levogirar <yonathanchu@gmail.com>
2021-01-28 12:18:02 -0500

    improved script

--- projects/scrapers/getPhoneES.php
+++ projects/scrapers/getPhoneES.php
@@ -8,70 +8,52 @@
  * Landline in Spain starts with 9
  */
 
-// Spain calling Code
+// Spain calling code & patterns
 $callingCodeES = '+34';
 $callingCodeES2 = "0034";
-
-$url = 'https://www.bolsoaurora.es'; // Target website
-$html = file_get_contents($url);  // Download the HTML page
-
 $patternNumberES = '/(?:([+]|00)34(.)?)?(6|9)[0-9]{1,}.[0-9]{1,}.[0-9]{1,}.[0-9]{1,}(.[0-9]{1,})?/'; // Look for a Spanish phone number
 $patternMobileES = '/(?:([+]|00)34)?(6|7)[0-9]{8}/'; // Verify Spanish mobile number
 $patternPhoneES = '/(?:([+]|00)34)?9[0-9]{8}/'; // Verify Spanish landline number
+
+$url = 'https://camdencoffeeroasters.com/contacto/'; // Target website
+$html = file_get_contents($url);  // Download the HTML page
 
 $dom = new DOMDocument();
 @$dom->loadHTML($html); // Symbol '@' added to prevent warning message
 
 // Get website text
-
-// // This does not work (?)
-// $bodyTexts = [];
-// $webBody = $dom->getElementsByTagName('body');
-// $bodyTexts = $webBody->textContent;
-// print_r($bodyTexts);
-
-// This works
 $bodyTexts = [];
 foreach ($dom->getElementsByTagName('body') as $body) {
 	$bodyTexts = $body->textContent;
 }
 
-// Find phone number & eliminate unnecessary characters
-preg_match_all($patternNumberES, $bodyTexts, $numberES);
-
+// Find phone number & sanitize results
+preg_match_all($patternNumberES, $texts, $numbersES);
+$numbersES = array_values(array_unique($numbersES[0])); // Remove duplicates
 $eliminate = ['.', ' ', '-']; // Characters to be replaced
-$numberES = implode(';', str_replace($eliminate, '', $numberES[0]));
+$numbersES = str_replace($eliminate, '', $numbersES); // Remove special characters
+
+// Standardize mobile numbers
+foreach ($numbersES as &$number) {
+	if (substr($number, 0, 3) === $callingCodeES) {
+		$number = $number;
+	} elseif (substr($number, 0, 4) === $callingCodeES2) {
+		$number = str_replace($callingCodeES2, $callingCodeES, substr($number, 0, 4)) . substr($number, 4, 9); // Change 0034612345678 to +34612345678
+	} else {
+		$number = $callingCodeES . $number; // Add +34 to mobile number
+	}
+}
+
+//Turn array into string
+$numbersES = implode(';', $numbersES);
 
 // Sort mobile and landline
-preg_match_all($patternMobileES, $numberES, $mobileES);
-preg_match_all($patternPhoneES, $numberES, $phoneES);
+preg_match_all($patternMobileES, $numbersES, $mobilesES);
+preg_match_all($patternPhoneES, $numbersES, $phonesES);
 
-$mobileES = $mobileES[0];
-$phoneES = $phoneES[0];
+$mobilesES = $mobilesES[0];
+$phonesES = $phonesES[0];
 
-foreach ($mobileES as &$mobile) {
-	if (substr($mobile, 0, 3) === $callingCodeES) {
-		$mobile = $mobile;
-	} elseif (substr($mobile, 0, 4) === $callingCodeES2) {
-		$mobile = str_replace($callingCodeES2, $callingCodeES, substr($mobile, 0, 4)) . substr($mobile, 4, 9); // Change 0034612345678 to +34612345678
-	} else {
-		$mobile = $callingCodeES . $mobile; // Add +34 to mobile number
-	}
-}
-
-foreach ($phoneES as &$phone) {
-	if (substr($phone, 0, 3) === $callingCodeES) {
-		$phone = $phone;
-	} elseif (substr($phone, 0, 4) === $callingCodeES2) {
-		$phone = str_replace($callingCodeES2, $callingCodeES, substr($phone, 0, 4)) . substr($phone, 4, 9); // Change 0034912345678 to +34912345678
-	} else {
-		$phone = $callingCodeES . $phone; // Add +34 to landline number
-	}
-}
-
-// Remove duplicate numbers
-$mobileES = array_values(array_unique($mobileES));
-$phoneES = array_values(array_unique($phoneES));
-
-print_r($mobileES);
-print_r($phoneES);
+print_r($mobilesES);
+echo '<br/><br/>';
+print_r($phonesES);
